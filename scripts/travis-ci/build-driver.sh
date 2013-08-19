1e12be4e92cdd86e1fcfdf3f35d4f632c9d40da5
Saesch di Vara <saeschdivara@gmail.com>
2013-08-19 16:28:26 -0300

    Now hopefully the right build tool will be used

--- scripts/travis-ci/build-driver.sh
+++ scripts/travis-ci/build-driver.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
 qmake -v
-qmake ./src/arangodb-driver.pro -r -spec linux-$CXX CONFIG+=debug
+CXX="g++"
+if [[ "$CXX" == "g++" ]]
+then
+  qmake ./src/arangodb-driver.pro -r -spec linux-g++ CONFIG+=debug
+else
+  qmake ./src/arangodb-driver.pro -r -spec linux-clang CONFIG+=debug
+fi
+
 make -j4
