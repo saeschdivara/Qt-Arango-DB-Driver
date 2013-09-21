67c835f164663ff0c5ac62662f9f1957fb6b2fd2
Saesch di Vara <saeschdivara@gmail.com>
2013-09-21 13:13:11 -0300

    This does find out where the compiliers are

--- scripts/travis-ci/build-driver.sh
+++ scripts/travis-ci/build-driver.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-which clang++
-clang --version
-
-ls -lh /usr/bin/gcc
-ls -lh /usr/bin/g++
-ls -lh /usr/bin/clang
-ls -lh /usr/bin/clang++
+#ls -lh /usr/local/bin/gcc
+#ls -lh /usr/local/bin/g++
+#ls -lh /usr/local/bin/clang
+#ls -lh /usr/local/bin/clang++
 
 qmake -v
 
 if [[ "$CXX" == "g++" ]]
 then
+  whereis gcc
+  whereis g++
+
+  gcc --version
   qmake ./src/arangodb-driver.pro -r -spec linux-g++ CONFIG+=debug CONFIG+=TRAVIS_CI_GCC
 else
+  whereis clang
+  whereis clang++
+
+  clang --version
   qmake ./src/arangodb-driver.pro -r -spec linux-clang CONFIG+=debug
 fi
 
