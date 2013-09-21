373425f591933cd692d3ab7098e030e57edab1c7
Saesch di Vara <saeschdivara@gmail.com>
2013-09-21 14:27:16 -0300

    Hopefully fixes gcc build

--- scripts/travis-ci/build-driver.sh
+++ scripts/travis-ci/build-driver.sh
@@ -8,15 +8,13 @@ mkdir ../arangodb-driver-dist/release
 
 if [[ "$CXX" == "g++" ]]
 then
-  g++ --version
   sudo update-alternatives --install /usr/local/bin/gcc gcc /usr/bin/gcc-4.7 70 --slave /usr/local/bin/g++ g++ /usr/bin/g++-4.7
-  g++ --version
 
-  qmake ./src/arangodb-driver.pro -r -spec linux-g++
+  qmake ./src/arangodb-driver.pro -r -spec linux-g++ CONFIG=DEBUG
 else
   sudo update-alternatives --install /usr/local/bin/clang clang /usr/bin/clang 70 --slave /usr/local/bin/clang++ clang++ /usr/bin/clang++
 
-  qmake ./src/arangodb-driver.pro -r -spec linux-clang
+  qmake ./src/arangodb-driver.pro -r -spec linux-clang CONFIG=DEBUG
 fi
 
-make -j4
+make -j6
