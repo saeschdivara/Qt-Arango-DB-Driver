e9ef7bc99bbd02d8f302b7457384f296239f21f2
Saesch di Vara <saeschdivara@gmail.com>
2013-09-21 14:23:36 -0300

    Should now have the right target in the project

--- scripts/travis-ci/build-driver.sh
+++ scripts/travis-ci/build-driver.sh
@@ -12,20 +12,11 @@ then
   sudo update-alternatives --install /usr/local/bin/gcc gcc /usr/bin/gcc-4.7 70 --slave /usr/local/bin/g++ g++ /usr/bin/g++-4.7
   g++ --version
 
-  qmake ./src/arangodb-driver.pro -r -spec linux-g++ CONFIG+=debug CONFIG+=TRAVIS_CI_GCC
+  qmake ./src/arangodb-driver.pro -r -spec linux-g++
 else
-  whereis clang
-  whereis clang++
-
-  ls -lh /usr/local/bin/clang
-  ls -lh /usr/local/bin/clang++
-
-  clang --version
   sudo update-alternatives --install /usr/local/bin/clang clang /usr/bin/clang 70 --slave /usr/local/bin/clang++ clang++ /usr/bin/clang++
-  sudo update-alternatives --config clang
-  clang --version
 
-  qmake ./src/arangodb-driver.pro -r -spec linux-clang CONFIG+=debug
+  qmake ./src/arangodb-driver.pro -r -spec linux-clang
 fi
 
 make -j4
