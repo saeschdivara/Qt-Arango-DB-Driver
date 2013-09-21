de8039bd37f34cb25b166921d0ec5fd47c8c68ef
Saesch di Vara <saeschdivara@gmail.com>
2013-09-21 13:46:40 -0300

    Now gcc build should work

--- scripts/travis-ci/build-driver.sh
+++ scripts/travis-ci/build-driver.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
-#ls -lh /usr/local/bin/gcc
-#ls -lh /usr/local/bin/g++
-#ls -lh /usr/local/bin/clang
-#ls -lh /usr/local/bin/clang++
-
 qmake -v
 
 if [[ "$CXX" == "g++" ]]
 then
-  whereis gcc
-  whereis g++
-
-  gcc --version
-  #sudo update-alternatives --install /usr/local/bin/gcc gcc /usr/bin/gcc-4.6 60 --slave /usr/local/bin/g++ g++ /usr/bin/g++-4.6
+  g++ --version
   sudo update-alternatives --install /usr/local/bin/gcc gcc /usr/bin/gcc-4.7 70 --slave /usr/local/bin/g++ g++ /usr/bin/g++-4.7
-  sudo update-alternatives --config gcc
-  gcc --version
+  g++ --version
 
   qmake ./src/arangodb-driver.pro -r -spec linux-g++ CONFIG+=debug CONFIG+=TRAVIS_CI_GCC
 else
   whereis clang
   whereis clang++
 
+  ls -lh /usr/local/bin/clang
+  ls -lh /usr/local/bin/clang++
+
   clang --version
+  sudo update-alternatives --install /usr/local/bin/clang clang /usr/bin/gcc-4.7 70 --slave /usr/local/bin/g++ g++ /usr/bin/g++-4.7
+  clang --version
+
   qmake ./src/arangodb-driver.pro -r -spec linux-clang CONFIG+=debug
 fi
 
